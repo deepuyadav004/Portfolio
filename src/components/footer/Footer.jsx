651bb35e39c435f48e10785428558e89b619b7c2
deepuyadav004 <deepuyadavze@gmail.com>
2022-08-27 15:23:09 +0530

    completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {BsTwitter} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>SNOW79</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='#'><BsTwitter/></a>
+        <a href='#'><BsLinkedin/></a>
+        <a href='#'><BsInstagram/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; SNOW79 all rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
